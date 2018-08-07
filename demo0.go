f3599f2d6f130d8aeedca5ffc3eb0f23ed72dbe0
Tong Sun <suntong@cpan.org>
2018-08-06 22:30:37 -0400

    - [+] add demo0.go staight from github.com/tucnak/telebot

---
+++ demo0.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+	"log"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		Token:  "TOKEN_HERE",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "hello world")
+	})
+
+	b.Start()
+}
